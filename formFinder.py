b0d0cf84754d04212287afd669e234df52d06b4c
dfink08 <defink8@gmail.com>
2014-02-07 14:31:44 -0500

    Added variable find
    This version finds all of the html files from the folder created by wget. It them scans all of those files for instances of "<input>" sections. It then looks for all instances of either "id" or "name" in order to gather all of the variable names. Then it looks for instances of "value." When it finds each of those, it puts them into a text file, which will be the dictionary used by configfileio to update form fields. I still need to clean this up and get rid of any excess code.

--- formFinder.py
+++ formFinder.py
@@ -6,44 +6,114 @@ import os,mimetypes,subprocess
 from os.path import join, getsize
 		
 pathName = input("Path of the html files: ")
-pathwalk = os.walk(pathName)
+
+htmlFiles = []
+
+variableNames = []
+variableValues = []
+configDict = pathName+"/configDict.txt"
+configDictfile = open(configDict,"w")
+
 for root, dirs, files in os.walk(pathName):
   print root
   
-  for name in files:
-    filePath = str(root+'/'+name)
+  for fileName in files:
+    filePath = str(root+'/'+fileName)
     proc = subprocess.Popen(["file",filePath],stdout=subprocess.PIPE)
     output = proc.stdout.read()
     
     if "HTML" in output:
-      print(name)
-
+      #htmlFiles.append(filePath)
+      
+      htmlFile = open(filePath, 'r')
+      fileText = htmlFile.read()
+      
+      print "FILE Path: " + filePath
+      inputStart = fileText.find("<input")
+      if inputStart == -1:
+	inputStart = fileText.find("<INPUT") #This is really ugly, find a more elegant solution.
+      
+      
+      #Scan the file looking for form code
+      # If find returns -1 (i.e. no more form or a form section never existed) quit
+      while inputStart != -1:
+	#print "Input start: ",inputStart
+	inputEnd = fileText[inputStart:].find(">")+inputStart+1
+	#print "Input End: ", inputEnd
+	#if inputEnd == 7:
+	  #inputEnd = fileText.find(">")+8
+	  #print "Input End: ", inputEnd
+	inputSection = fileText[inputStart:inputEnd] 
+	#print "Input Text: ", inputSection
+	
+	
+	#Look for the instance of "name" or "id" in the form section
+	# This is the label for a changeable field
+	nameIndex=0
+	if "name=" in inputSection:
+	  nameIndex = inputSection.find("name=")
+	elif "id=" in inputSection:
+	  nameIndex = inputSection.find("id=")
+	#print "Name Index: ",nameIndex	      
+	nameS = nameIndex+inputSection[nameIndex:].find("\"")+1
+	nameE = inputSection[nameS:].find("\"")+nameS
+	
+	name = inputSection[nameS:nameE]
+	
+	variableNames.append(name)
+	configDictfile.write(fileName[:-5]+"_"+name+"=")
+		      
+	# After finding the name of the variable, look for its current value
+	valueIndex = inputSection.find("value=")
+	#print "Value Index: ", valueIndex	      
+	valueS = valueIndex+inputSection[valueIndex:].find("\"")+1
+	#print "Value start: ", valueS+valueIndex
+	valueE = inputSection[valueS:].find("\"")+valueS
+	#print "Value end: " , valueE+valueS
+	value = inputSection[valueS:valueE]
+	
+	variableValues.append(value)
+	configDictfile.write(value+"\n")
+      
+	inputSection = inputSection[valueE:]
+	
+	# After finding all of the variable fields, look for any more form sections that
+	# may be in the page
+	fileText = fileText[inputEnd:]
+	inputStart = fileText.find("<input")
+	if inputStart == -1:
+	  inputStart = fileText.find("<INPUT") #This is really ugly, find a more elegant solution.
                 
+
+#print variableNames
+#print variableValues
+configDictfile.close()
+        
 # packetFile = open(fileName, 'r')
 
 # fileText = htmlFile.read()
-# formStart = fileText.find("<form>" or "<FORM>")
+# inputStart = fileText.find("<form>" or "<FORM>")
 
 # #Scan the file looking for form code
 # #   If find returns -1 (i.e. no more form or a form section never existed) quit
-# while formStart != -1:
-    # formEnd = fileText.find("</form>" or "</FORM>")+7
+# while inputStart != -1:
+    # inputEnd = fileText.find("</form>" or "</FORM>")+7
 
-    # formText = fileText[formStart:formEnd]
-    # print(formText)
+    # inputSection = fileText[inputStart:inputEnd]
+    # print(inputSection)
     
-    # htmlFileName = formText[nameIndexS:nameIndexE]
+    # htmlFileName = inputSection[nameIndexS:nameIndexE]
     # print(htmlFileName,'\n')
-    # fileText = fileText[formEnd:]
+    # fileText = fileText[inputEnd:]
     
-    # formStart = fileText.find("<form>" or "<FORM>")
+    # inputStart = fileText.find("<form>" or "<FORM>")
 
     # # Check if file already exists
     # # if it doesn't, make it and make it generic
     # try:
         # # Open file for exclusive creation
         # htmlFile = open(htmlFileName,"x")
-        # htmlFile.write(formText)
+        # htmlFile.write(inputSection)
         # print("\nFile creation successful")
 
         # htmlFile.close()
