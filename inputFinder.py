ab2dabed07fcc99ce94844602ac9cc4d96133a20
dfink08 <defink8@gmail.com>
2014-03-13 10:00:16 -0400

    Works but ugly
    At the moment this is some very convoluted code. I really need to make sure this gets cleaned up and that it no longer requires a hard coded IP. Everything should be self sufficient.

--- inputFinder.py
+++ inputFinder.py
@@ -5,6 +5,7 @@
 
 import os,mimetypes,subprocess
 from os.path import join, getsize
+import httplib
 		
 #pathName = input("Path of the html files: ")
 pathName = '/root/Desktop/koyo_emulator'
@@ -14,25 +15,72 @@ configDict = pathName+"/config.txt"
 configDictfile = open(configDict,"w")
 dictionary = {}
 
+responses = {}
+h1 = httplib.HTTPConnection('10.1.0.112')
+h1.request("GET", "/")
+r1 = h1.getresponse() 
+allHeaders = r1.getheaders()
+headerString = ""
+for header in allHeaders:
+  headerString = headerString+header[0]+':'+header[1]+'\r\n'
+		  
+toWrite = ("HTTP/1.0" if r1.version == 10 else "HTTP/1.1")+" "
+toWrite += str(r1.status)+" "
+toWrite += str(r1.reason)+'\r\n'
+toWrite += headerString+'\r\n'
+if "10.1.0.112" in toWrite:
+  toWrite = toWrite.replace("10.1.0.112","127.0.0.1")
+#if "Re" in str(r1.reason):
+print toWrite
+responses['/'] = toWrite
+
 # os.walk will walk the entire directory of files and create a generator object that can be iterated
 for root, dirs, files in os.walk(htmlPath):
- 
+  #print "actually runs this loop"
+  
+  #print "Root: ",root
   for fileName in files:
+    #print " actually runs second loop"
     filePath = str(root+'/'+fileName)
+    preamble, realPath = root.split('112',1)
+    #print "Full Path: ",filePath
+    #print "File name: ",fileName
     # this line runs the "file" bash command. stdout=subprocess.PIPE keeps the output from printing
     #	to the screen and allows you to put the output into a variable
     proc = subprocess.Popen(["file",filePath],stdout=subprocess.PIPE)
     output = proc.stdout.read()
-    
+    #print " The output of running file: ",output
     # If the file command identifies the file as html then "HTML" will be somewhere in the output
-    if "HTML" in output:
+    if ("HTML" in output) or ("SGML" in output) :
+      #print "  recognizes as html"
+      
+      h1.request("GET", "/"+fileName)
+      print "Get works"
+      #responses['/'+fileName] = h1.getresponse() 
+      r1 = h1.getresponse() 
+      print r1.msg
+      allHeaders = r1.getheaders()
+      headerString = ""
+      for header in allHeaders:
+	headerString = headerString+header[0]+':'+header[1]+'\r\n'
+		  
+      toWrite = ("HTTP/1.0" if r1.version == 10 else "HTTP/1.1")+" "
+      print toWrite
+      toWrite += str(r1.status)+" "
+      print toWrite
+      toWrite += str(r1.reason)+'\r\n'
+      toWrite += headerString+'\r\n'
+      if "10.1.0.117" in toWrite:
+	toWrite = toWrite.replace("10.1.0.117","127.0.0.1")
+      print toWrite
+      responses['/'+fileName] = toWrite
       
       htmlFile = open(filePath, 'r+')
       fileText = htmlFile.read()
       
       newFileText = fileText
       
-      #print "FILE Path: " + filePath
+      #print " FILE Path: " + filePath
       inputStart = fileText.find("<input")
       if inputStart == -1:
 	inputStart = fileText.find("<INPUT") #This is really ugly, find a more elegant solution.
@@ -80,7 +128,7 @@ for root, dirs, files in os.walk(htmlPath):
 	#	therefore whe you go to retrieve the value it will look for what is right of the equal
 	#	sign, but instead of finding just the value, it will also include part of the name
 	if nameIndex != 0:
-	  configDictfile.write(newName+'=')
+	  configDictfile.write(newName+'@')
 	
 	if (varType == "checkbox") or (varType == "radio"):
       
@@ -100,10 +148,10 @@ for root, dirs, files in os.walk(htmlPath):
 	
 	#print "The Value: ", inputSection[valueS:valueE]
 	if nameIndex != 0:
-	  print "The Value: ", inputSection[valueS:valueE]
+	  #print "The Value: ", inputSection[valueS:valueE]
 	  newInputSection = inputSection
 	  newInputSection = inputSection[:valueS]+'${'+fileName[:-5]+"_"+name+'}'+inputSection[valueE:]
-	  print "New input" , newInputSection, "\n"
+	  #print "New input" , newInputSection, "\n"
 	  
 	  newFileText = newFileText.replace(fileText[inputStart:inputEnd],newInputSection)
 	  
@@ -119,14 +167,45 @@ for root, dirs, files in os.walk(htmlPath):
 	if inputStart == -1:
 	  inputStart = fileText.find("<INPUT") #This is really ugly, find a more elegant solution.      
       #print newFileText 
-      newHtmlFile = open(newHtmlPath+fileName, 'w')
+      if not os.path.exists(newHtmlPath+realPath+'/'):
+	os.makedirs(newHtmlPath+realPath+'/')
+      #if fileName.endswith('.html') or fileName.endswith('.asp'):
+      newHtmlFile = open(newHtmlPath+realPath+'/'+fileName, 'w')
+	#print "  New html path: ",newHtmlPath
+	#print "  New realPath: ", realPath
+	#print "  New file name: ", '/'+fileName
+      #else: 
+	#newHtmlFile = open(newHtmlPath+realPath+'/'+fileName+'.html', 'w')
+	#print "  New html path: ",newHtmlPath
+	#print "  New realPath: ", realPath
+	#print "  New file name: ", fileName+'.html'
       #htmlFile.seek(0,0)
+      newHtmlFile.write(newFileText)
+      newHtmlFile.close()
+      htmlFile.close()
+    else:
+      if not os.path.exists(newHtmlPath+realPath+'/'):
+	os.makedirs(newHtmlPath+realPath+'/')
+      htmlFile = open(filePath, 'r+')
+      fileText = htmlFile.read()
+      newFileText = fileText
+      newHtmlFile = open(newHtmlPath+realPath+'/'+fileName, 'w')
       newHtmlFile.write(newFileText)
       newHtmlFile.close()
       htmlFile.close()
 #print dictionary
 configDictfile.close()
-        
+
+varFile = open(pathName+"/variableFile.py", "w")
+varFile.write("dictionary = ")
+varFile.write(str(dictionary))
+varFile.write("\n")
+
+varFile.write("responses = ")
+varFile.write(str(responses))
+varFile.write("\n")
+varFile.close()
+
 #TODO: This code currently makes a lot of assumptions about how each html file will be formatted.
 #	More testing must be done to ensure that this will work on any web site
 
