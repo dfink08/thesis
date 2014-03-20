f381c3f9108cb9e6572856506dd62c1ef3541f52
dfink08 <defink8@gmail.com>
2014-03-20 17:02:28 -0400

    Fixed issue with getting non-html files
    This version is still very ugly, but I moved the get requests out tof the section that looks for html files. This way it now properly does a get for all file types so that the web server can actually handle them.

--- inputFinder.py
+++ inputFinder.py
@@ -3,20 +3,23 @@
 #	It writes this dictionary to a text file called configDict.txt
 # Uses python 3.3.3
 
-import os,mimetypes,subprocess
+import os,mimetypes,subprocess, sys
 from os.path import join, getsize
 import httplib
-		
+import time
+
+ipaddress = sys.argv[1]
+print "IP: ",ipaddress
 #pathName = input("Path of the html files: ")
-pathName = '/root/Desktop/koyo_emulator'
-htmlPath = pathName+'/10.1.0.112'
+pathName = '/root/Desktop/ab_emulator'
+htmlPath = pathName+'/'+ipaddress
 newHtmlPath = pathName+'/web_server/'
 configDict = pathName+"/config.txt"
 configDictfile = open(configDict,"w")
 dictionary = {}
 
 responses = {}
-h1 = httplib.HTTPConnection('10.1.0.112')
+h1 = httplib.HTTPConnection(ipaddress)
 h1.request("GET", "/")
 r1 = h1.getresponse() 
 allHeaders = r1.getheaders()
@@ -28,59 +31,58 @@ toWrite = ("HTTP/1.0" if r1.version == 10 else "HTTP/1.1")+" "
 toWrite += str(r1.status)+" "
 toWrite += str(r1.reason)+'\r\n'
 toWrite += headerString+'\r\n'
-if "10.1.0.112" in toWrite:
-  toWrite = toWrite.replace("10.1.0.112","127.0.0.1")
-#if "Re" in str(r1.reason):
-print toWrite
+if ipaddress in toWrite:
+  toWrite = toWrite.replace(ipaddress,"127.0.0.1")
+
 responses['/'] = toWrite
 
 # os.walk will walk the entire directory of files and create a generator object that can be iterated
 for root, dirs, files in os.walk(htmlPath):
-  #print "actually runs this loop"
   
-  #print "Root: ",root
   for fileName in files:
-    #print " actually runs second loop"
+    time.sleep(.001)
+    #print "File name: ", fileName
     filePath = str(root+'/'+fileName)
-    preamble, realPath = root.split('112',1)
-    #print "Full Path: ",filePath
-    #print "File name: ",fileName
+    preamble, realPath = root.split('117',1)
+    
     # this line runs the "file" bash command. stdout=subprocess.PIPE keeps the output from printing
     #	to the screen and allows you to put the output into a variable
     proc = subprocess.Popen(["file",filePath],stdout=subprocess.PIPE)
     output = proc.stdout.read()
-    #print " The output of running file: ",output
-    # If the file command identifies the file as html then "HTML" will be somewhere in the output
-    if ("HTML" in output) or ("SGML" in output) :
-      #print "  recognizes as html"
-      
-      h1.request("GET", "/"+fileName)
-      print "Get works"
-      #responses['/'+fileName] = h1.getresponse() 
-      r1 = h1.getresponse() 
-      print r1.msg
-      allHeaders = r1.getheaders()
-      headerString = ""
-      for header in allHeaders:
+    #print "File Path: ", filePath
+    #print "File Name: ", fileName
+    #h1.request("GET", "/"+fileName)
+    h1.request("GET", realPath+'/'+fileName)
+    #print "Get request: ", realPath+'/'+fileName
+    r1 = h1.getresponse() 
+
+    allHeaders = r1.getheaders()
+    headerString = ""
+    for header in allHeaders:
 	headerString = headerString+header[0]+':'+header[1]+'\r\n'
 		  
-      toWrite = ("HTTP/1.0" if r1.version == 10 else "HTTP/1.1")+" "
-      print toWrite
-      toWrite += str(r1.status)+" "
-      print toWrite
-      toWrite += str(r1.reason)+'\r\n'
-      toWrite += headerString+'\r\n'
-      if "10.1.0.117" in toWrite:
-	toWrite = toWrite.replace("10.1.0.117","127.0.0.1")
-      print toWrite
-      responses['/'+fileName] = toWrite
+    toWrite = ("HTTP/1.0" if r1.version == 10 else "HTTP/1.1")+" "
+    toWrite += str(r1.status)+" "
+    toWrite += str(r1.reason)+'\r\n'
+    toWrite += headerString+'\r\n'
+    if ipaddress in toWrite:
+	toWrite = toWrite.replace(ipaddress,"127.0.0.1")
+    #if fileName.endswith(".gif"):
+      #print "Gif response: ", toWrite
+    responses[realPath+'/'+fileName] = toWrite
+    #responses[realPath] = toWrite
+    # If the file command identifies the file as html then "HTML" will be somewhere in the output
+    if ("HTML" in output) or ("SGML" in output) :
+      #if "home" in fileName:
+        #print "File name: ", fileName
+        #print "real path: ", realPath
+      
       
       htmlFile = open(filePath, 'r+')
       fileText = htmlFile.read()
       
       newFileText = fileText
       
-      #print " FILE Path: " + filePath
       inputStart = fileText.find("<input")
       if inputStart == -1:
 	inputStart = fileText.find("<INPUT") #This is really ugly, find a more elegant solution.
@@ -94,18 +96,15 @@ for root, dirs, files in os.walk(htmlPath):
 	
 	inputSection = fileText[inputStart:inputEnd] 	
 	newInputSection = inputSection
-	
-	
+		
 	typeIndex = inputSection.find("type=")
 	typeS = typeIndex+inputSection[typeIndex:].find("\"")+1
 	typeE = inputSection[typeS:].find("\"")+typeS
 	
 	varType = inputSection[typeS:typeE]
-	#print "Input Type: ",varType
 	
 	#Look for the instance of "name" or "id" in the form section
-	# This is the label for a changeable field
-	
+	# This is the label for a changeable field	
 	nameIndex=0
 	if "name=" in inputSection:
 	  nameIndex = inputSection.find("name=")
@@ -114,6 +113,7 @@ for root, dirs, files in os.walk(htmlPath):
 	
 	  name = inputSection[nameS:nameE]
 	  newName = fileName[:-5]+"_"+name
+	
 	elif "id=" in inputSection:
 	  nameIndex = inputSection.find("id=")
 	  nameS = nameIndex+inputSection[nameIndex:].find("\"")+1
@@ -122,8 +122,6 @@ for root, dirs, files in os.walk(htmlPath):
 	  name = inputSection[nameS:nameE]
 	  newName = fileName[:-5]+"_"+name
 	      
-	
-	
 	#NOTE: this format can cause issues. Some files have an equal sign in the name
 	#	therefore whe you go to retrieve the value it will look for what is right of the equal
 	#	sign, but instead of finding just the value, it will also include part of the name
@@ -145,13 +143,10 @@ for root, dirs, files in os.walk(htmlPath):
 	  valueE = inputSection[valueS:].find("\"")+valueS
 	  
 	  value = inputSection[valueS:valueE]
-	
-	#print "The Value: ", inputSection[valueS:valueE]
+
 	if nameIndex != 0:
-	  #print "The Value: ", inputSection[valueS:valueE]
 	  newInputSection = inputSection
 	  newInputSection = inputSection[:valueS]+'${'+fileName[:-5]+"_"+name+'}'+inputSection[valueE:]
-	  #print "New input" , newInputSection, "\n"
 	  
 	  newFileText = newFileText.replace(fileText[inputStart:inputEnd],newInputSection)
 	  
@@ -166,20 +161,12 @@ for root, dirs, files in os.walk(htmlPath):
 	inputStart = fileText.find("<input")
 	if inputStart == -1:
 	  inputStart = fileText.find("<INPUT") #This is really ugly, find a more elegant solution.      
-      #print newFileText 
+     
       if not os.path.exists(newHtmlPath+realPath+'/'):
 	os.makedirs(newHtmlPath+realPath+'/')
+      
       #if fileName.endswith('.html') or fileName.endswith('.asp'):
       newHtmlFile = open(newHtmlPath+realPath+'/'+fileName, 'w')
-	#print "  New html path: ",newHtmlPath
-	#print "  New realPath: ", realPath
-	#print "  New file name: ", '/'+fileName
-      #else: 
-	#newHtmlFile = open(newHtmlPath+realPath+'/'+fileName+'.html', 'w')
-	#print "  New html path: ",newHtmlPath
-	#print "  New realPath: ", realPath
-	#print "  New file name: ", fileName+'.html'
-      #htmlFile.seek(0,0)
       newHtmlFile.write(newFileText)
       newHtmlFile.close()
       htmlFile.close()
@@ -196,7 +183,7 @@ for root, dirs, files in os.walk(htmlPath):
 #print dictionary
 configDictfile.close()
 
-varFile = open(pathName+"/variableFile.py", "w")
+varFile = open(pathName+"/variableFile.py", "a")
 varFile.write("dictionary = ")
 varFile.write(str(dictionary))
 varFile.write("\n")
